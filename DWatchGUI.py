2f76b075804b9504c952d8f5b3e1825b8f0fbffb
LeeHyungJoo <>
2019-12-09 11:58:28 +0200

    removed dependency from each other

--- DWatchGUI.py
+++ DWatchGUI.py
@@ -14,7 +14,7 @@ class DWatchGUI:
     self.eventhandler = eventhandler
     self.parent = parent
     self.start_holding_button = 0
-    self.backgroundChronoMode = False
+    self.chrono_mode = False
     self.display_time_mode = True
 
     self.handleEventOn()
@@ -48,52 +48,48 @@ class DWatchGUI:
 
   def topLeftPressed(self):
     print "topLeftPressed"
-    self.backgroundChronoModeOff()
 
   def topLeftReleased(self):
     print "topLeftReleased"
-    self.eventhandler.event("changeMode")
+    if self.display_time_mode:
+      self.turnOnChronoMode()
+      self.turnOffTimeDisplayMode()
+      self.refreshChronoDisplay()
+
+    else:
+      self.eventhandler.event("display_time_mode")
+      self.turnOffChronoMode()
+      self.turnOnTimeDisplayMode()
+      self.refreshTimeDisplay()
+
     
   def bottomRightPressed(self):
     self.start_holding_button = datetime.datetime.now()
     self.eventhandler.event("bottomRightPressed")
-
-    if self.display_time_mode is False:
+    if self.chrono_mode:
       self.eventhandler.event("initChrono")
 
   def chronoRunning(self):
       for i in range(25):
         self.increaseChronoByOne()
-
-  def backgroundChrono(self):
-    if(self.backgroundChronoMode == True):
-      time.sleep(0.25)
-      self.chronoRunning()
-      self.backgroundChrono()
-
-  def startThreadBackgroundChrono(self):
-    if self.backgroundChronoMode == True:
-      thread.start_new_thread(self.backgroundChrono, ())
-
-  def backgroundChronoModeOn(self):
-    self.backgroundChronoMode = True
-
-  def backgroundChronoModeOff(self):
-    self.backgroundChronoMode = False
+      if self.chrono_mode:
+        self.refreshChronoDisplay()
 
   def bottomRightReleased(self):
     self.eventhandler.event("released")
-    diff = datetime.datetime.now() - self.start_holding_button
-    holding_duration = round(float(diff.total_seconds()), 1)
+    if self.display_time_mode:
+      diff = datetime.datetime.now() - self.start_holding_button
+      holding_duration = round(float(diff.total_seconds()), 1)
 
-    if holding_duration >= 2:
-      self.eventhandler.event("finishEdit")
-    elif holding_duration >= 1.5:
-      self.eventhandler.event("editTime")
-    self.start_holding_button = 0
+      if holding_duration >= 2:
+        self.eventhandler.event("finishEdit")
+      elif holding_duration >= 1.5:
+        self.eventhandler.event("editTime")
+      self.start_holding_button = 0
 
   def bottomLeftPressed(self):
-    self.eventhandler.event("resetChrono")
+    if self.chrono_mode:
+      self.eventhandler.event("resetChrono")
     self.eventhandler.event("increase")
     self.eventhandler.event("setAlarm")
 
@@ -125,7 +121,6 @@ class DWatchGUI:
  
   def increaseTimeByOne(self):
     self.GUI.increaseTimeByOne()
-    self.refreshTimeDisplay()    
 
   def resetChrono(self):
     self.GUI.resetChrono()
@@ -177,22 +172,26 @@ class DWatchGUI:
 
   # Update running time for every second
   def updateRunningTime(self):
+    self.increaseTimeByOne()
     if self.display_time_mode:
-      self.GUI.increaseTimeBySecond()
       self.refreshTimeDisplay()
 
-    while self.display_time_mode == False:
-      self.GUI.increaseTimeBySecond()
-      time.sleep(1)
 
   # Stop waiting for edit, expired for 5 seconds
   def waitingEditExpired(self):
     self.eventhandler.event("finishEdit")
 
+
   # Turn on/off Time Display Mode
-  def turnOffTimeDisplayMode(self):
-    self.display_time_mode = False
-    thread.start_new_thread(self.updateRunningTime, ())
 
   def turnOnTimeDisplayMode(self):
     self.display_time_mode = True
+
+  def turnOffTimeDisplayMode(self):
+    self.display_time_mode = False
+
+  def turnOnChronoMode(self):
+    self.chrono_mode = True
+
+  def turnOffChronoMode(self):
+    self.chrono_mode = False
